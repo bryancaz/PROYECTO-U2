a00df2aa6886670866e3f78fcccfeeb2824f06b2
bryancaz <103477342+bryancaz@users.noreply.github.com>
2022-07-05 01:34:44 -0500

    Add alt on img src

--- src/180600.js
+++ src/180600.js
@@ -15,7 +15,7 @@ export default function Nombre () {
         <p>MATRICULA: 180600
           </p>  
         </p> 
-        <img src='https://scontent-qro1-1.xx.fbcdn.net/v/t1.6435-9/175592646_174032541236737_5279875056717616143_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_eui2=AeFsc-RYTAklgfHSc7_IDw-lvXcc0DJwz3i9dxzQMnDPeLuxOPB2K-PMukVhRyZ6sqxI-kXwBZEqS5RMiSpyMSjw&_nc_ohc=ritS30QzgI4AX8Um02n&_nc_ht=scontent-qro1-1.xx&oh=00_AT-yCs2MIh2wiRqmbdS8LgVMywH4MNz9E6Pv-nndb5eMcQ&oe=62E7FC7B' className='fotoBryan'></img>
+        <img src='https://scontent-qro1-1.xx.fbcdn.net/v/t1.6435-9/175592646_174032541236737_5279875056717616143_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_eui2=AeFsc-RYTAklgfHSc7_IDw-lvXcc0DJwz3i9dxzQMnDPeLuxOPB2K-PMukVhRyZ6sqxI-kXwBZEqS5RMiSpyMSjw&_nc_ohc=ritS30QzgI4AX8Um02n&_nc_ht=scontent-qro1-1.xx&oh=00_AT-yCs2MIh2wiRqmbdS8LgVMywH4MNz9E6Pv-nndb5eMcQ&oe=62E7FC7B' alt =""></img>
         
       </div>
   
