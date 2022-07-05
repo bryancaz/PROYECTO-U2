d142d1cd9d30cb2a044cc4d7c6cc2a228ec6fd11
bryancaz <103477342+bryancaz@users.noreply.github.com>
2022-07-05 01:04:14 -0500

    We use the route path

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './web';
-//CREAR CONSTANTE ROOT
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Pokemon from './PokemonID';
+import Alumno from './180600';
+import Rutas from './Rutes';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> 
+    <Rutas />
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/pokemon/:id" element={<Pokemon />} />
+        <Route path="/180600" element={<Alumno />} />
+      </Routes>
+    
+    </BrowserRouter>
   </React.StrictMode>
 );
-
 reportWebVitals();
